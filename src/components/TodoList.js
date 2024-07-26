5e75ce2d4502563bf45e4781dca092c29e9bc41a
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-26 10:37:31 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,18 +5,16 @@ const TodoList = ({ todos, handleComplete }) => {
     <>
       <h2>Child Component</h2>
       <ul>
-{todos.map(todo => (
-  <li key={todo.id}>
-    {todo.text}
-    {!todo.completed && (
-      <button 
-        onClick={() => handleComplete(todo.id)}
-      >
-        'Complete'
-      </button>
-    )}
-  </li>
-))}
+        {todos.map(todo => (
+          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+            {todo.text}
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
       </ul>
     </>
   );
