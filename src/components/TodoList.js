f46230c561f8f747f148b964c80f8e02b0d54317
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-06 00:00:35 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,13 @@ import React from 'react';
 const TodoList = ({ todos, handleComplete }) => {
   return (
     <ul>
-      {todos.map(todo => (
+      {todos.map((todo) => (
         <li key={todo.id}>
-          <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            {todo.text}
-          </span>
+          {todo.text}
           {!todo.completed && (
-            <button onClick={() => handleComplete(todo.id)}>Complete</button>
+            <button onClick={() => handleComplete(todo.id)}>
+              Complete
+            </button>
           )}
         </li>
       ))}
