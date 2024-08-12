f8154f95aa25810af46a9ac6174f9993f0b9e84c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-12 20:48:34 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,8 +5,9 @@ function TodoList({ todos, onComplete }) {
     <ul>
       {todos.map(todo => (
         <li key={todo.id}>
-          <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            {todo.text}
+            <span>{todo.text}</span>
+          <span style={{ display: todo.completed ? 'none' : '' }}>
+              {todo.button}
           </span>
           {!todo.completed && (
             <button onClick={() => onComplete(todo.id)}>Complete</button>
