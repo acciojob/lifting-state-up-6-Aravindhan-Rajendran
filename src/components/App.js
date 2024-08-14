b6419bf5c77fd159adb755279bd1fb8b9eaed14b
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-14 15:21:09 +0530

    comit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
-import TodoList from './TodoList';
-
-function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build a React App', completed: false },
-    { id: 3, text: 'Deploy the App', completed: false }
-  ]);
-
-  const handleComplete = (id) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? { ...todo, completed: true } : todo
-    ));
-  };
-
+import React from "react";
+ 
+import Todo from "./Todo.js";
+ 
+import "./../styles/App.css";
+ 
+const App = () => {
+ 
   return (
+ 
     <div>
-      <h1>Todo List</h1>
-      <TodoList todos={todos} onComplete={handleComplete} />
+ 
+      <h1>Parent Component</h1>
+ 
+      <Todo />
+ 
     </div>
+ 
   );
-}
-
+ 
+};
+ 
 export default App;
